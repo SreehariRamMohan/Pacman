ba104edaaab78f3fcdde245b4e26b06d0620f31a
nkane236 <nkane236@CHS-312-08.chs.win.fuhsd.org>
2018-05-01 10:06:42 -0700

    Added a new class and methods

---
+++ src/Testing.java
@@ -0,0 +1,26 @@
+
+public class Testing {
+	private int nums;
+	private String names;
+	private int count;
+	
+	public Testing() {
+		nums = 0;
+		names = "Neil";
+		count = 0;
+	}
+	
+	public Testing(int num, String nam, int cnt) {
+		nums = num;
+		names = nam;
+		count = cnt;
+	}
+	
+	public int addOne() {
+		return nums+1;
+	}
+	
+	public int addTwo() {
+		return count+2;
+	}
+}
