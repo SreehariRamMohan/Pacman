385e864fef818f70fa583424186ad3ce02315649
sreeharirammohan@gmail.com <wizmathhelp@gmail.com>
2018-05-07 16:39:50 -0700

    Starting fresh adding Character

--- src/Character.java
+++ src/Character.java
@@ -1,50 +1,21 @@
 import javafx.scene.paint.Color;
 
-public abstract class Character {
-	private int x;
-	private int y;
-	private double speed = 1;
-	private Color color = null;
-	
-	public Character(int x, int y, Color color) {
-		super();
-		this.x = x;
-		this.y = y;
-		this.color = color;
+public class Character extends Actor {
+
+	@Override
+	public void act(long now) {
+		// TODO Auto-generated method stub
+		
 	}
 	
-	public abstract void move(int newX, int newY);
-	public abstract void die();
-
-	public int getX() {
-		return x;
+	public void moveTo(int x, int y) {
+		
 	}
-
-	public void setX(int x) {
-		this.x = x;
+	
+	public void die() {
+		
 	}
+	
+	
 
-	public int getY() {
-		return y;
-	}
-
-	public void setY(int y) {
-		this.y = y;
-	}
-
-	public double getSpeed() {
-		return speed;
-	}
-
-	public void setSpeed(double speed) {
-		this.speed = speed;
-	}
-
-	public Color getColor() {
-		return color;
-	}
-
-	public void setColor(Color color) {
-		this.color = color;
-	}
 }
--- src/Detectable.java
+++
@@ -1,5 +0,0 @@
-
-public interface Detectable {
-	public void onDetect(Character c);
-	public void onDetect(Food f);
-}
